449708b85e44f7d72b5b70bb0c3d8c7223ba26c7
Abhigyan Mukherjee <abhigyan.mukherjee@finica.co.za>
2026-04-23 00:34:09 +0530

    v1.0.0

--- src/main/java/com/ecom/User.java
+++ src/main/java/com/ecom/User.java
@@ -1,10 +1,34 @@
 package com.ecom;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor// we can directly use this annotation without using constructor
+@AllArgsConstructor
+@Entity(name = "user_table")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+
+//    public User() {
+//    } // default constructor for jpa
+//
+//    public User(Long id, String firstName, String lastName) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+
+
+
 }
--- src/main/java/com/ecom/UserController.java
+++ src/main/java/com/ecom/UserController.java
@@ -10,19 +10,20 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
 
     private final UserService userService;
 
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers (){
 
         return new ResponseEntity<>(userService.fetchAllUsers(),HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser (@PathVariable Long id){
 //        User user = userService.fetchUser(id);
 //        if (user == null)
@@ -34,13 +35,13 @@ public class UserController {
                     .orElseGet(()-> ResponseEntity.notFound().build());
 
     }
-    @PostMapping ("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser (@RequestBody User user){
         userService.addUser(user);
         return new ResponseEntity<>("User added successfully", HttpStatus.CREATED);
     }
 
-    @PutMapping ("/api/users/{id}")
+    @PutMapping ("/{id}")
     public ResponseEntity<String> updateUser (@PathVariable Long id,
                                             @RequestBody User updatedUser){
         boolean updated = userService.updateUser(id,updatedUser);
